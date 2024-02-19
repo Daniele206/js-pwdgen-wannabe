ebdf54d3af46d2427f328b9d37baa695a02774af
Daniele206 <danieledeieso2004@gmail.com>
2024-02-19 15:26:09 +0100

    [add] notSecurePassword consol log

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const firstName = prompt ('Scrivi il tuo nome');
+
+console.log(firstName);
+
+const lastName = prompt ('Scrivi il tuo cognome');
+
+console.log(lastName);
+
+const fullName = firstName + lastName;
+
+console.log(fullName);
+
+const favouriteColor = prompt ('Scrivi il tuo colore preferito');
+
+console.log(favouriteColor);
+
+const notSecurePassword = fullName + favouriteColor + '24';
+
+console.log(notSecurePassword);
